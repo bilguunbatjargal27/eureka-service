aef75f9976697a7c5fbf9344263345b994fee3cb
Bilguun Batjargal <bilguunbatjargal27@gmail.com>
2020-09-20 21:54:43 -0500

    f

--- src/main/java/cs/mum/edu/orangeteam/compro/ComproApplication.java
+++ src/main/java/cs/mum/edu/orangeteam/compro/ComproApplication.java
@@ -2,7 +2,11 @@ package cs.mum.edu.orangeteam.compro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaServer
 @SpringBootApplication
 public class ComproApplication {
 
